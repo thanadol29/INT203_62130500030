2f71eee7f5004329c5242d1889fe1ded87bf84da
Thanadol <Thanadol.29@mail.kmutt.ac.th>
2021-02-03 10:05:46 +0700

    [Note book]

---
+++ 62130500030_groupwork_2/main.js
@@ -0,0 +1,9 @@
+    const app = {
+        data() {
+            return {
+                msg: 'Hello, Vue3'
+            }
+        }
+
+    }
+    var mountedApp = Vue.createApp(app).mount('#app')
